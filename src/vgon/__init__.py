01ac244756341d08fa1c8738aa1e2aa9d60165fe
hazy <88533544+leo2www@users.noreply.github.com>
2026-03-13 19:11:38 +0800

    pypi (#5)

---
+++ src/vgon/__init__.py
@@ -0,0 +1,1 @@
+from vgon._version import __version__
---
+++ src/vgon/examples/gap/__init__.py
@@ -0,0 +1,9 @@
+from .lo import E2Lo
+from .locc import E2Locc
+from .ns import __initialize_Ns__, Ns
+
+__all__ = [
+    "__initialize_Ns__",
+    "E2Lo",
+    "E2Locc",
+]
